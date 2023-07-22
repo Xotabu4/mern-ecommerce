90eab52e658c189d4ecba039146987b409470bcb
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2023-07-22 14:26:11 +0300

    Disabled S3 storage

--- server/utils/storage.js
+++ server/utils/storage.js
@@ -1,31 +1,33 @@
-const AWS = require('aws-sdk');
+// const AWS = require('aws-sdk');
 
-const keys = require('../config/keys');
+// const keys = require('../config/keys');
+
 
 exports.s3Upload = async image => {
-  let imageUrl = '';
-  let imageKey = '';
+  let imageUrl = undefined;
+  let imageKey = undefined;
 
-  if (image) {
-    const s3bucket = new AWS.S3({
-      accessKeyId: keys.aws.accessKeyId,
-      secretAccessKey: keys.aws.secretAccessKey,
-      region: keys.aws.region
-    });
+  // Mocked to not waste time and money on S3 setup
+  // if (image) {
+  //   const s3bucket = new AWS.S3({
+  //     accessKeyId: keys.aws.accessKeyId,
+  //     secretAccessKey: keys.aws.secretAccessKey,
+  //     region: keys.aws.region
+  //   });
 
-    const params = {
-      Bucket: keys.aws.bucketName,
-      Key: image.originalname,
-      Body: image.buffer,
-      ContentType: image.mimetype,
-      ACL: 'public-read'
-    };
+  //   const params = {
+  //     Bucket: keys.aws.bucketName,
+  //     Key: image.originalname,
+  //     Body: image.buffer,
+  //     ContentType: image.mimetype,
+  //     ACL: 'public-read'
+  //   };
 
-    const s3Upload = await s3bucket.upload(params).promise();
+  //   const s3Upload = await s3bucket.upload(params).promise();
 
-    imageUrl = s3Upload.Location;
-    imageKey = s3Upload.key;
-  }
+  //   imageUrl = s3Upload.Location;
+  //   imageKey = s3Upload.key;
+  // }
 
   return { imageUrl, imageKey };
 };
